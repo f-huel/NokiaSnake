3974fdc84ce819e46587a03ca1b01b6922159861
Mehdi El Khallouki <mehdiek03@gmail.com>
2024-02-08 11:50:41 +0100

    Manual merge

---
+++ src/script.ts
@@ -0,0 +1,18 @@
+import "phaser";
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload(this: Phaser.Scene):void {};
+function create(this: Phaser.Scene):void {};
+function update():void {};
